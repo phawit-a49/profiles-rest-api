8a9028033dd35cd2f54001c3e2b13d5b446964b0
phawit-a49 <phawit@a49.co.th>
2024-10-18 07:41:06 +0000

    Added custom user profile model, manager and migrations

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from profiles_api.models import UserProfile
 
-# Register your models here.
+# Define the UserProfileAdmin
+class UserProfileAdmin(admin.ModelAdmin):
+    # These fields should correspond to the fields used for the `autocomplete_fields`
+    search_fields = ['name', 'email']  # Replace with actual fields in your UserProfile model
+
+# Register the UserProfile model and associate it with UserProfileAdmin
+admin.site.register(UserProfile, UserProfileAdmin)
