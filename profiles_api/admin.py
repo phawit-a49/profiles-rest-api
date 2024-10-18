49ed9083624ceb735375804ed8ff93e8303938a1
phawit-a49 <phawit@a49.co.th>
2024-10-18 18:50:23 +0700

    Added viewset to profiles api

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -5,6 +5,7 @@ from profiles_api.models import UserProfile
 class UserProfileAdmin(admin.ModelAdmin):
     # These fields should correspond to the fields used for the `autocomplete_fields`
     search_fields = ['name', 'email']  # Replace with actual fields in your UserProfile model
-
+    list_display = ['email', 'is_staff']
+    list_filter = ['is_staff',]
 # Register the UserProfile model and associate it with UserProfileAdmin
 admin.site.register(UserProfile, UserProfileAdmin)
